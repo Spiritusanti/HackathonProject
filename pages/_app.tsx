59376b0046da16079cd01632574fec482bf69ae5
Spiritusanti <mccrackenjacob@gmail.com>
2021-08-30 13:26:59 -0700

    working on manifest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,9 +14,9 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="Description" content="rock paper scissors 2D game" />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
         <meta name="theme-color" content="black" />
-        <link rel="manifest" href="../public/manifest.json" />
-        <link rel="apple-touch-icon" href="../public/apple-touch-icon.png" />
-        <link rel="shortcut icon" href="../public/favicon.ico" type="image/x-icon" />
+        <link rel="manifest" href="manifest.json" />
+        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
       </Head>
       <Component {...pageProps} />
     </html>
