20022c0bcdfb0fc4c9ebaae83a32b03eb5e8459f
Spiritusanti <mccrackenjacob@gmail.com>
2021-08-30 13:20:04 -0700

    updated manifest and head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="Description" content="rock paper scissors 2D game" />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
+        <meta name="theme-color" content="black" />
         <link rel="manifest" href="../public/manifest.json" />
         <link rel="apple-touch-icon" href="../public/apple-touch-icon.png" />
         <link rel="shortcut icon" href="../public/favicon.ico" type="image/x-icon" />
